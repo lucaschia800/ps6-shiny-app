b9e8e354884593330051b3a91363dc41d478c3dc
lucaschia800 <lucaschia800@gmail.com>
2023-02-28 10:16:57 -0800

    start

--- app.R
+++ app.R
@@ -1,50 +1,26 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+library(tidyverse)
 library(shiny)
+temp_data <- read_delim("UAH-lower-troposphere-long.csv.bz2")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+  titlePanel("ps6 shiny app"),
+  p("This is an app designed to relay certain data trends on global temperature changes."),
+  tabsetPanel(type="tabs",
+              tabPanel("Intro"),
+              tabPanel(("Plot 1"),p("this is where the plots will go"))),
+  
+  
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
 
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  
 }
 
 # Run the application 
