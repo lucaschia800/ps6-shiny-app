eeb475364d9b389272abc1f035aa3ea72d7d5bae
lucaschia800 <lucaschia800@gmail.com>
2023-03-01 11:40:31 -0800

    update

--- app.R
+++ app.R
@@ -9,16 +9,48 @@ ui <- fluidPage(
   p("This is an app designed to relay certain data trends on global temperature changes."),
   tabsetPanel(type="tabs",
               tabPanel("Intro"),
-              tabPanel(("Plot 1"),p("this is where the plots will go"))),
-  
-  
+              tabPanel(("Plot 1"),
+                       p("this is where the plots will go"),
+                       sidebarLayout(
+                         sidebarPanel(checkboxGroupInput("regions", label = ("Region(s) to Display"), 
+                                                         choices = list("globe_ocean"= "globe_ocean","nh"="nh"),
+                                                         selected = "globe_ocean"),
+                                      radioButtons("color", label = ("Plot Color"),
+                                                   choices = list("Red" = "red", "Blue" = "blue", "Green" = "green"), 
+                                                   selected = "red"),
+                                      sliderInput("year_range", label = h3("Year Range"), min = 1978, 
+                                                  max = 2023, value = c(1978, 2023))),
+                         mainPanel(plotOutput("plot"))))
 
-
+              
+  ),
+  
 
 )
 
-# Define server logic required to draw a histogram
+
 server <- function(input, output) {
+
+  
+  
+
+  output$plot <- renderPlot({
+    temp_data_mean <- temp_data %>% 
+      group_by(year) %>% 
+      summarize(avg_temp=mean(temp)) %>% 
+      filter(year>=input$year_range[1],year<=input$year_range[2])
+    
+    temp_data_rmean <- temp_data %>% 
+      group_by(year,region) %>% 
+      summarize(avg_temp=mean(temp)) %>% 
+      filter(region %in% input$regions)
+    
+    ggplot()+geom_point(data=temp_data_mean,aes(year,avg_temp),color=input$color)+
+      geom_point(data=temp_data_rmean,aes(year,avg_temp,col=region))
+      
+  }
+    
+  )
 
   
 }
